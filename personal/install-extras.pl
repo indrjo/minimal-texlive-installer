e1acd5f80f79eac6758436ec863b405d4e355b23
indrjo <dedej.indrjo@gmail.com>
2022-12-19 21:38:04 +0100

    A couple of tiny typos corrected; packages added to the list

--- personal/install-extras.pl
+++ personal/install-extras.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl -W
 
-# This tiny Perl script install some basic packages after you have got your
+# This tiny Perl script installs some basic packages after you have got your
 # minimal TeX Live. The list of packages starts directly below __DATA__.
 # Feel free to edit this list as your personal needs. Blank lines and lines
 # starting by "#" are not taken into account, so use them to organize your
@@ -15,11 +15,21 @@ while (my $ln = <DATA>) {
 
 __DATA__
 
-# base packages for me
+# packages we cannot live without
+texlive-scripts-extra
+latex-bin
+texdoc
+texliveonfly
+
+# classes
 suftesi
+
+# base packages
 fontspec
 polyglossia
-mycrotype
+babel
+hyphen-{english,german,french,italian,greek}
+microtype
 csquotes
 hyperref
 biblatex
