1480538695f04b408dba2c0d19f9b0ddb0d3621e
indrjo <dedej.indrjo@gmail.com>
2022-12-19 21:28:00 +0100

    install-extras.pl incorporates the list of packages to install; comments and instructions

--- personal/install-extras.pl
+++ personal/install-extras.pl
@@ -1,11 +1,41 @@
 #!/usr/bin/env perl -W
 
-open(my $inh, '<', 'extra-installs.txt') or die "$!\n";
-while (my $ln = <$inh>) {
+# This tiny Perl script install some basic packages after you have got your
+# minimal TeX Live. The list of packages starts directly below __DATA__.
+# Feel free to edit this list as your personal needs. Blank lines and lines
+# starting by "#" are not taken into account, so use them to organize your
+# list and add comments.
+
+while (my $ln = <DATA>) {
   if ($ln !~ /^\s*(#|$)/) {
     $ln =~ s/^\s+|\s+$//g;
     system "tlmgr install $ln";
   }
 }
-close($inh);
+
+__DATA__
+
+# base packages for me
+suftesi
+fontspec
+polyglossia
+mycrotype
+csquotes
+hyperref
+biblatex
+pgf
+
+# fonts
+libertine
+sourcedepro
+
+# mathematical fonts
+libertinust1math
+mathalfa
+mathtools
+MnSymbol
+
+# other mathematical stuff
+tikzcd
+commutative-diagrams
 
