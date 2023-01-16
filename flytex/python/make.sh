2c8101114c611a6f8038ed0754ece0bb3d2573bd
indrjo <dedej.indrjo@gmail.com>
2023-01-16 20:26:50 +0100

    on android install flytex in $PREFIX/bin

--- flytex/python/make.sh
+++ flytex/python/make.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
-here=~/.local/bin
+set -e
+
+if [ `uname -o` == Android ]
+  then here=$PREFIX/bin
+  else here=~/.local/bin
+fi
 [ -d $here ] || mkdir -p $here
 cp ./flytex.py $here/flytex
+chmod u+x $here/flytex
 
