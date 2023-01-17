9c9a5810512dbfc16d2ea6ad9127c064c5eac658
indrjo <dedej.indrjo@gmail.com>
2023-01-17 08:29:01 +0100

    slight changes

--- flytex/python/make.sh
+++ flytex/python/make.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-# You can install the *python flyex* on Android too.
-if [ `uname -o` == Android ]
-  then here=$PREFIX/bin
-  else here=~/.local/bin
-fi
-[ -d $here ] || mkdir -p $here
-cp ./flytex.py $here/flytex
-chmod u+x $here/flytex
+OS=`uname -o` # the operating system
+prefix=`[ $OS == Android ] && echo $PREFIX/bin || echo ~/.local/bin`
+
+[ -d $prefix ] || mkdir -p $prefix
+cp --preserve=mode ./flytex.py $prefix/flytex
+#chmod u+x $prefix/flytex
 
