32193b95b9e94f540c4d4a2b76acd4ef1d89e110
indrjo <dedej.indrjo@gmail.com>
2023-01-18 09:06:29 +0100

    wtf ?! I used == instead of = in sh scripts...

--- flytex/python/make.sh
+++ flytex/python/make.sh
@@ -3,9 +3,10 @@
 set -e
 
 OS=`uname -o` # the operating system
-prefix=`[ $OS == Android ] && echo $PREFIX/bin || echo ~/.local/bin`
+prefix=`[ $OS = Android ] && echo $PREFIX/bin || echo ~/.local/bin`
 
 [ -d $prefix ] || mkdir -p $prefix
-cp --preserve=mode ./flytex.py $prefix/flytex
-#chmod u+x $prefix/flytex
+cp ./flytex.py $prefix/flytex
+chmod u+x $prefix/flytex
+
 
