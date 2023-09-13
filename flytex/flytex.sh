69a69754eb7079917dc064a408edef84b4fdc7af
indrjo <dedej.indrjo@gmail.com>
2023-09-13 21:31:24 +0200

    combinations of `grep` and `sed` are now replaced by more elegant `perl` one-liners; more descriptive comments...

--- flytex/flytex.sh
+++ flytex/flytex.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 #
-# DESCRIPTION AND USAGE
+# DESCRIPTION
 #
 # This small script install TeX Live packages on the fly, that is:
 #
@@ -9,6 +9,8 @@
 # 2. It inspects corresponding log file for missing files
 # 3. Interrogate CTAN to get the names of the packages owning them, and
 #    install them right away.
+#
+# USAGE
 #
 # If you are used to write, for instance
 #
@@ -33,35 +35,60 @@ opts="$@"
 # The log file is just the main file's name with ".log" in place of ".tex".
 flog="${ftex%.tex}.log"
 
+# Communicate the user with a small decoration, in order to  distinguish it
+# from the output of the employed TeX engine. 
+_say () {
+  echo -e "\n(flytex) $1\n"
+}
+
+# Drop some error message end halt the run of this script.
 _die () {
-  echo -e "\n(flytex-error) $1\n"; exit
+  echo -e "\n(flytex-error) $1\n"
+  exit 1
 }
 
-# Take a log file as argument and get all the names of the missing files.
+# Read the TeX log file and list all the missing files. More precisely, the
+# function looks for strings of the form
+#
+#  file `SOME_FILENAME' not found
+#
+# to extract SOME_FILENAME. One typical line where it occurs looks like:
+#
+#  ! LaTeX error: File `SOME_FILENAME' not found.
+#
+# We hope this pattern encompasses as many cases as possible.
 _get_missing_fnames () {
-  grep -iP 'file \S+ not found' "$1" | sed -E "s/.*\`([^']+)'.*/\1/"
+  perl -lne "/file \`([^']+)' not found/i && print \$1" "$1"
 }
 
-# Interrogate CTAN and get the list of the packages owning some file.
-_tlmgr_search () {
-  tlmgr search --global --file "/$1" | grep -P ':$' | sed 's/:$//'
+# Parse the output of
+#
+#  $ tlmgr search --global --file "/FILENAME"
+#
+# to extract the names of the resulting packages.
+# !!! The output of tlmgr is not exactly "machine readable", resulting in
+# !!! this part of code being rather delicate.
+_get_package_names () {
+  perl -lne "/([^:]+):$/ && print \$1"
 }
 
-export -f _tlmgr_search
-
-# Run the TeX command. We may leave users the bother to hit Enter every
-# time the compilation halts for some reason. Some missing files results
-# entire packages not being installed, thus the compilation will halt for
-# other reasons as well.
-# !!! The script takes care of at most 50 halts: after this amount, it is
-# !!! up to you to hit the Enter key until the TeX machinery terminates.
+# Run some TeX command. We may leave users the bother to hit Enter every
+# time the compilation halts for some reason (for instance, for some file
+# cannot be found), but we choose to not do so here.
+# !!! There is a limited tolerance though: the script can handle up to 50
+# !!! halts by itself. After this amount it is up to the user to hit Enter
+# !!! until TeX terminates. I am considering to use `timeout` here...
 perl -e 'print "\n"x50' | "$engine" "$opts" "$ftex"
 
-# Test internet connection, it will be needed.
+# Time for the actual flytex...
+_say "interrogating CTAN and getting missing packages..."
+
+# Test internet connection, it will be needed. The get the repos' urls, we
+# take advantage of the output of `tlmgr repository list`.
 tlmgr repository list | grep -oP 'http\S+' \
   | xargs wget -q --spider || _die "cannot reach CTAN!"
 
-# All the program in one line...
+# All the program in "one" line...
 _get_missing_fnames "$flog" \
-  | xargs -I % sh -c '_tlmgr_search %' \
-  | xargs tlmgr install
+  | xargs -I % sh -c 'tlmgr search --global --file "/%"' \
+  | _get_package_names | xargs tlmgr install
