4ff6ce426ef33f7eb48ebd8ff8760427994fca75
indrjo <dedej.indrjo@gmail.com>
2023-09-22 21:01:59 +0200

    the default `max_print_line` may introduce newlines in the middle of the names; `get_missing_fnames` is modified to avoid such pesky \n-s

--- flytex/flytex.py
+++ flytex/flytex.py
@@ -1,112 +1,108 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#from dataclasses import dataclass
-from re          import compile, search, findall, IGNORECASE, MULTILINE
-from subprocess  import run
+#
+# DESCRIPTION
+#
+# This small script installs TeX Live packages on the fly, that is:
+#
+# 1. It attempts the processing of them main TeX file to create a document
+# 2. It inspects corresponding log file for missing files.
+# 3. Interrogate CTAN to get the names of the packages owning them, and
+#    install them right away.
+#
+# USAGE
+#
+# If you are used to write, for instance
+#
+#   $ lualatex --shell-escape --synctex=1 main.tex
+#
+# to create your documents, then you have just to
+#
+#   $ flytex lualatex main.tex
+#
+# That is: prepend flytex and forget the options. Indeed, this program is
+# rather a workaround: use it for the first when you suspect your TeX Live
+# might not have all the needed stuff and then return to your habits.
+#
+
+from re import findall, I, M
+from subprocess import run
 import sys
 
-# Just print a message with some small decoration.
+# Just print a message with some small decoration, to distinguish it from
+# the output of TeX.
 def say(text):
   print(f':: {text}')
 
 # There are two kinds of error messages here: some just tell something is
 # wrong but they won't crush the program, whereas others will immediately
-# halt the run. By default, error messages are not fatal.
+# halt the execution. By default, error messages are not fatal.
 def say_error(text, fatal=False):
-  print(f'!! {text}', file=sys.stderr)
+  print(f'!!! {text}', file=sys.stderr)
   if fatal:
     sys.exit()
 
 # The USAGE string. It will be used for a description of the program later.
-usage = 'USAGE: flytex engine file.tex [options]'
+usage = 'USAGE: flytex engine file.tex'
 
-# Here is the form of the string we are looking for in our logs.
-missing_pattern = compile(r"file `([^']+)' not found", IGNORECASE)
-
-# This function take a string: if has the form of missing_pattern, then it
-# will extract a filename, otherwise a None is returned.
-def get_missing_fname(string):
-  try:
-    return search(missing_pattern, string).group(1)
-  except AttributeError:
-    # !!! If there is no match, search returns a None.
-    return None
-
-# Provided a log file produced by some TeX engine, extract all the names of
-# the missing files, as indicated by the smaller functions above.
+# Read a TeX log file and list all the missing files.
 def get_missing_fnames(texlogfile):
-  fnames = []
-  with open(texlogfile) as loglines:
-    for line in loglines:
-      fname = get_missing_fname(line)
-      if not fname is None:
-        fnames.append(fname)
+  with open(texlogfile) as file:
+    # !!! What if the log file is very big? Could that ever happen?
+    contents = file.read().replace('\n', '')
+    fnames = findall(r"['\"\`]([^'\"\`]+)['\"\`] not found", contents, I)
   return fnames
 
 # Given a filename, get from CTAN all the names of the packages owning it.
-# For the purpouse we will make the host system run
+# More precisely, parse the output of
 #
-#  $ tlmgr search --global --file /fname
+#  $ tlmgr search --global --file "/FILENAME"
 #
-# !!! Pay attention to the "/". Should I introduce the possible to choose a
-# !!! less strict search?
+# to extract the names of the resulting packages.
+# !!! The output of tlmgr is not exactly "machine readable", resulting in
+# !!! this part of code being rather delicate.
 def tlmgr_search(fname):
   search_proc = run(['tlmgr', 'search', '--global', '--file', '/'+fname],
                     capture_output=True)
   if search_proc.returncode == 0:
     stdout_str = search_proc.stdout.decode()
-    return findall(r'\n([^:]+):\n', stdout_str, MULTILINE)
+    return findall(r'\n([^:]+):\n', stdout_str, M)
   else:
     say_error(search_proc.stderr.decode())
 
-# Run the TeX machinery, inspect the log and install the needed packages.
-def flytex(engine, ftex, options):
-  # Create of a pdf document. This part will never halt if the command run
-  # halts for some reason. It may halt for other reasons though, but in the
-  # main the function flytex will be wrapped in a try-except block.
-  run([engine] + options + [ftex], input=(50*'\n').encode())
-
-  # Slurp the log and isolate the names of the absent files.
+# Run some TeX command. We may leave users the bother to hit Enter every
+# time the compilation halts for some reason (for instance, for some file
+# cannot be found), but we choose to not do so here.
+# !!! There is a limited tolerance though: the script can handle up to 50
+# !!! halts by itself. After this amount it is up to the user to hit Enter
+# !!! until TeX terminates.
+def flytex(engine, ftex):
+  run([engine, ftex], input=(50*'\n').encode())
   ftexlog = ftex.replace('.tex', '.log')
   missing_fnames = get_missing_fnames(ftexlog)
-
-  # See whether the files are contained in some packages. Since they come
-  # from a complaint of missing files, these packages are not installed.
   if missing_fnames == []:
     say('no missing files!')
   else:
-    # Determine the list of packages to be installed.
     packages = []
     for fname in missing_fnames:
-      packages = packages + tlmgr_search(fname)
-
-    # And install them. The failed installation of some package will not
-    # prevent the others coming next in the list from being installed.
-    installed, not_installed = [], []
-    for pkg in packages:
-      installation = run(['tlmgr', 'install', pkg])
-      if installation.returncode == 0:
-        installed.append(pkg)
+      packages_for_fname = tlmgr_search(fname)
+      if packages_for_fname is None or packages_for_fname == []:
+        say_error('orphan file: ' + fname)
       else:
-        not_installed.append(pkg)
-
-    # A summary of the process is best to be present. If some installation
-    # failed, it is best the user knows.
-    print('installed: ' +  ', '.join(installed))
-    if not_installed == []:
-      say('all packages installed!')
-    else:
-      say_error('NOT installed: ' +  ', '.join(not_installed), True)
+        packages = packages + packages_for_fname
+    installation = run(['tlmgr', 'install'] + packages)
+    if installation.returncode != 0:
+      say_error(installation.stderr)
 
 if __name__ == '__main__':
   # !!! The arguments of flytex come from the command-line without any kind
-  # !!! of sophistication. Non argparse or simila modules for now.
+  # !!! of sophistication. No argparse or similar modules for now.
   try:
-    engine, file, *options = sys.argv[1:]
+    engine, ftex = sys.argv[1:3]
+    try:
+      flytex(engine, ftex)
+    except Exception as some_exception:
+      say_error(some_exception)
   except ValueError:
-    say_error(f'{usage}', True)
-  try:
-    flytex(engine, file, options)
-  except Exception as some_exception:
-    say_error(some_exception, True)
+    say_error(f'{usage}')
